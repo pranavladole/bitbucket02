abf88ab334805b7571a068632d2793417e7cfbeb
pranavladole <pranavladole98@gmail.com>
2026-04-11 12:31:45 +0530

    main

--- a.java
+++ a.java
@@ -1,7 +1,5 @@
 public class a {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("hotfix pay");
     }
 }
