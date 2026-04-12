95d2266a1fb0483800119390cb4788c5621996cd
pranavladole <pranavladole98@gmail.com>
2026-04-12 10:22:24 +0530

    cleanup

--- a.java
+++ a.java
@@ -1,9 +1,5 @@
 public class a {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Hello, World! main");
-        System.out.println("Hello, World! main");
-        System.out.println("Hello, World! develop");
-        System.out.println("Hello, World! develop");
     }
 }
