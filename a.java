81853cec311f62b401adc53146a7bd48e0eb80f6
pranavladole <pranavladole98@gmail.com>
2026-04-12 12:22:04 +0530

    feature2

--- a.java
+++ a.java
@@ -1,6 +1,6 @@
 public class a {
     public static void main(String[] args) {
-        System.out.println("Hello, World! feature1");
-        System.out.println("Hello, World! develop merged");
+        System.out.println("Hello, World! feature2");
+        System.out.println("Hello, World! feature2");
     }
 }
