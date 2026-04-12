cb7d1d3094e715dbb2795c01f2a61215a91e0790
pranavladole <pranavladole98@gmail.com>
2026-04-12 12:00:24 +0530

    feature2

--- a.java
+++ a.java
@@ -1,8 +1,5 @@
 public class a {
     public static void main(String[] args) {
-        System.out.println("Hello, World! develop");
         System.out.println("Hello, World! feature2");
-        System.out.println("Hello, World! feature2 test");
-        System.out.println("Hello, World! feature2 test2");
     }
 }
