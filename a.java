c2a9b6c29411c8a02c7fb3f4ab958550bd259a28
pranavladole <pranavladole98@gmail.com>
2026-05-28 17:49:18 +0530

    st

--- a.java
+++ a.java
@@ -2,5 +2,7 @@ public class a {
     public static void main(String[] args) {
         System.out.println("Hello, World! develop");
         System.out.println("Hello, World! feature1 i am in middle of work...");
+
+        System.out.println("Hello, World! feature1 working with stash..");
     }
 }
